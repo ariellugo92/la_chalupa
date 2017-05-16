0e6cd48018e851e678e33ad8025b671aa02867a5
ariellugo92 <ariellugo92resp@gmail.com>
2017-05-15 22:01:42 -0600

    terminando interfaz principal

--- src/LaChalupaIA.java
+++ src/LaChalupaIA.java
@@ -1,9 +1,12 @@
+
+import Vista.MenuPrincipal;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lachalupaia;
+
 
 /**
  *
@@ -15,7 +18,7 @@ public class LaChalupaIA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new MenuPrincipal().setVisible(true);
     }
     
 }
---
+++ src/Pojos/OpcionesIniciales.java
@@ -0,0 +1,40 @@
+package Pojos;
+
+/**
+ *
+ * @author ariellugo
+ */
+public class OpcionesIniciales {
+    
+    private int numero_jugadores;
+    private int numero_tablas;
+    private int forma_juego;
+
+    public int getNumero_jugadores() {
+        return numero_jugadores;
+    }
+
+    public void setNumero_jugadores(int numero_jugadores) {
+        this.numero_jugadores = numero_jugadores;
+    }
+
+    public int getNumero_tablas() {
+        return numero_tablas;
+    }
+
+    public void setNumero_tablas(int numero_tablas) {
+        this.numero_tablas = numero_tablas;
+    }
+
+    public int getForma_juego() {
+        return forma_juego;
+    }
+
+    public void setForma_juego(int forma_juego) {
+        this.forma_juego = forma_juego;
+    }
+
+    public OpcionesIniciales() {
+    }
+    
+}
